dc0b3fccc3f26b9ac3fc9f6631f3a8323ccde7db
Oguzhan Can <oguzhancan2009@gmail.com>
2020-05-15 19:56:45 +0300

    Prototype Finished
    Prototype Finished

---
+++ Prototype/Prototype/Cavalry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Cavalry:Army
+    {
+        
+        public Cavalry() //constructor
+        {
+            this.type = "Atlı Birlik";
+        }
+        
+        public override Army Clone()
+        {
+            return MemberwiseClone() as Cavalry;
+        }
+        public override int calcPower() //toplam gücü hesaplama
+        {
+            this.allpow = power * number;
+            return allpow;
+        }
+    }
+}
---
+++ Prototype/Prototype/Infantry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Infantry : Army
+    {
+        public Infantry() //constructor
+        {
+            this.type = "Piyade";
+        }
+
+        public override Army Clone()
+        {
+            return MemberwiseClone() as Infantry;
+        }
+        public override int calcPower() //hesaplama
+        {
+            this.allpow = power * number;
+            return allpow;
+
+        }
+    }
+}
