9d253c93c5f1e005064d10d16d90936a078fb07e
Oguzhan Can <oguzhancan2009@gmail.com>
2020-05-17 00:02:25 +0300

    Builder Finished
    Builder Finished

---
+++ Bridge/Bridge/Implementor/ColorPicker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Implementor
+{
+    public abstract class ColorPicker
+    {
+        public abstract void ColorPick(string carbrand);
+    }
+}
---
+++ Bridge/Bridge/RefinedAbstraction/Ford.cs
@@ -0,0 +1,15 @@
+﻿using Bridge.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.RefinedAbstraction
+{
+    public class Ford : CarPicker
+    {
+        public override void MakeChoice()
+        {
+            _colorPicker.ColorPick("Ford");
+        }
+    }
+}
