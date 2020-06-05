65d126709b16abac62be2473ed62c6489a038bf5
Oguzhan Can <oguzhancan2009@gmail.com>
2020-06-06 00:55:38 +0300

    Refactoring
    Refactoring

--- Proxy/Proxy/ProxyService.cs
+++ Proxy/Proxy/ProxyService.cs
@@ -7,47 +7,28 @@ namespace Proxy
     class ProxyService:IProductService
     {
         string username, pass;
-        int entry;
         public bool authProcess { get; private set; }
         private Products _products = new Products();
         public ProxyService(string username,string pass)
         {
             this.username = username;
             this.pass = pass;
-            entry = 0;
-        }
-       
-        bool Authorization()
-        {
-            if (entry == 0)
+
+            if (username == "ahmet" && pass == "1234")
             {
-                entry++;
-                if (username == "ahmet" && pass == "1234")
-                {
-                    Console.WriteLine("Başarıyla Giriş Yapıldı.\n");
-                    authProcess = true;
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("Hatalı şifre girişi\n");
-                    authProcess = false;
-                    entry--;
-                    return false;
-                }
-               
+                Console.WriteLine("Başarıyla Giriş Yapıldı.\n");
+                authProcess = true;
             }
             else
             {
-                return true;
+                Console.WriteLine("Hatalı şifre girişi\n");
+                authProcess = false;
             }
-           
-        } 
-
-
+        }
+       
         public List<string> getProducts()
         {
-            Authorization();
+            //Authorization();
             if (authProcess)
             {
                 Console.WriteLine("Ürünlerin Listesi\n");
@@ -61,7 +42,7 @@ namespace Proxy
         }
         public List<string> removeProduct(int ad)
         {
-            Authorization();
+            //Authorization();
             if (authProcess)
             {
                 Console.WriteLine("\nAlmak istediğiniz ürünü seçin\n");
